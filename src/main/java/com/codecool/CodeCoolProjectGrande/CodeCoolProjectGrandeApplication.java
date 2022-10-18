298ccaccdd50d6dcf81e5bab3a8abf626e17f7e3
KZwolski <kkzwolski@gmail.com>
2022-10-19 00:26:19 +0200

    Spring Boot initialization

---
+++ src/main/java/com/codecool/CodeCoolProjectGrande/CodeCoolProjectGrandeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.CodeCoolProjectGrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoolProjectGrandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCoolProjectGrandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/CodeCoolProjectGrande/CodeCoolProjectGrandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.CodeCoolProjectGrande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCoolProjectGrandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
