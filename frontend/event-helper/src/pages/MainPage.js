b5ba16cb4117d5b53acf66524e5ce73aae7a8a56
KZwolski <kkzwolski@gmail.com>
2022-11-03 00:38:45 +0100

    Main Page route added

---
+++ frontend/event-helper/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import logo from "../logo-jasne.png";
+import Navbar from "../components/Navbar";
+
+export default function MainPage(){
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    )
+
+}
