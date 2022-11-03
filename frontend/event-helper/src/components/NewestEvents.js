ba171112bd40926c379a01f870640e4fcb24587c
KZwolski <kkzwolski@gmail.com>
2022-11-03 22:46:16 +0100

    Created Tiles for displaying Newest Events on main page

---
+++ frontend/event-helper/src/components/NewestEvents.js
@@ -0,0 +1,17 @@
+export default function NewestEvents() {
+    return (
+        <div className="event-tile">
+        <div className="event-photo-tile">
+            <div className="event-rating-tile">
+                <div className="star">★</div>
+                <div className="rating">4.4</div>
+            </div>
+        </div>
+        <div className="event-name-tile">Tomorrowland</div>
+        <div className="event-location-tile">
+            <span className="material-symbols-outlined location-symbol">location_on</span>
+            <div className="location-text">Kraków</div>
+        </div>
+    </div>
+    )
+}
--- frontend/event-helper/src/pages/MainPage.js
+++ frontend/event-helper/src/pages/MainPage.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import NewestEvents from "../components/NewestEvents";
 
-export default function MainPage(){
-    return(
+export default function MainPage() {
+    return (
         <div>
             <Navbar/>
+            <h1>Newest Events</h1>
         </div>
     )
 
