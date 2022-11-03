c3460fa2d13d9129453fd15fa30f3058a6ebbbf7
KZwolski <kkzwolski@gmail.com>
2022-11-03 14:17:26 +0100

    Welcome Page gif modified

--- frontend/event-helper/src/pages/WelcomePage.js
+++ frontend/event-helper/src/pages/WelcomePage.js
@@ -1,11 +1,11 @@
 import logo from "../assets/logo-jasne.png";
 
-export default function WelcomePage(){
-    return(
-        <div>
-            <header className="App-header">
-            </header>
-            <a href={"/home"}><img src={logo} alt="Event Helper"/></a>
+export default function WelcomePage() {
+    return (
+        <div className="welcome-page">
+            <div className="welcome-page-gif-section">
+                <a href={"/home"}><img className="welcome-page-logo" src={logo} alt="Event Helper"/></a>
+            </div>
         </div>
     )
 
