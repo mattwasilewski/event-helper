674af5b63452afa16e2aca7f517db0cb96735255
KZwolski <kkzwolski@gmail.com>
2022-11-04 13:25:57 +0100

    Fixed Security Access

--- src/main/java/com/codecool/CodeCoolProjectGrande/user/SecurityConfig.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/user/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.codecool.CodeCoolProjectGrande.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -39,20 +40,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/homepage","/v2/api-docs",
-                        "/configuration/ui",
-                        "/swagger-resources/",
-                        "/configuration/security",
-                        "/swagger-ui/",
-                        "/webjars/**")
-//                .hasRole("ADMIN")
-                .permitAll()
+                .antMatchers("/**")
+                .permitAll().anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/homepage", true)
-                .permitAll();
+                .csrf().disable();
         return http.build();
     }
 
