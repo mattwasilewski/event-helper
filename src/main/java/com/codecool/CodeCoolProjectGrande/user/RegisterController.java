99aaa29748f60ec04fa8df8b5277bc84a3b8c4f6
BartoszGrabowski1 <heksargonh@gmail.com>
2022-11-14 13:34:08 +0100

    user details saving in db
    Former-commit-id: 73c2a8acd9c3d25e917cdb8aa9f61e3b88079edf

--- src/main/java/com/codecool/CodeCoolProjectGrande/user/RegisterController.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/user/RegisterController.java
@@ -1,4 +1,27 @@
 package com.codecool.CodeCoolProjectGrande.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@CrossOrigin
 public class RegisterController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public RegisterController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity registerAccount(@RequestBody User user){
+        System.out.println(user);
+        userRepository.save(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
