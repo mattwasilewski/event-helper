4f6253af075e4dcdbf961c2499b230103f9865f8
KZwolski <kkzwolski@gmail.com>
2022-11-28 23:41:45 +0100

    Calendar changed from react-calendar to react-big-calendar

--- frontend/event-helper/src/components/CalendarTile.js
+++ frontend/event-helper/src/components/CalendarTile.js
@@ -1,19 +1,56 @@
-import {useState} from "react";
-import Calendar from 'react-calendar';
+import {Calendar, dateFnsLocalizer, momentLocalizer} from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, {useState} from "react";
+import moment from 'moment';
 
-export default function CalendarTile(){
 
-    const [date, setDate] = useState(new Date());
+const localizer = momentLocalizer(moment)
 
+const events = [
+    {
+        title: "Event1",
+        id: 2,
+        allDay: true,
+        start: new Date(2022, 10, 16),
+        end: new Date(2022, 10, 19)
+    },
+    {
+        title: "Event2",
+        id: 3,
+        allDay: true,
+        start: new Date(2022, 10, 29),
+        end: new Date(2022, 10, 29)
+    },
+    {
+        title: "Event3",
+        id: 1,
+        allDay: true,
+        start: new Date(2022, 10, 27),
+        end: new Date(2022, 10, 27)
+    }
+
+]
+
+
+export default function CalendarTile() {
+
+
+    console.log(events);
     return (
         <div className="calendar-tile">
             <h1 className="header">Events Calendar</h1>
             <div className="calendar-container">
-                <Calendar onChange={setDate} value={date}/>
+                <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end"
+                          style={{height: 500, width: "800px"}}></Calendar>
+                {/*<Calendar onChange={setDate} value={date}/>*/}
             </div>
-            <div className="text-center">
-                {/*Selected date: {date.toDateString()}*/}
-            </div>
+            {/*<div className="text-center">*/}
+            {/*    /!*Selected date: {date.toDateString()}*!/*/}
+            {/*</div>*/}
         </div>
     );
 }
