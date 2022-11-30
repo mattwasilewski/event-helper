d62c0a37a919585dadb12cba0cf168e937b9fca1
KZwolski <kkzwolski@gmail.com>
2022-11-30 22:40:13 +0100

    added new fetch to get all events, added useEffect hook

--- frontend/event-helper/src/components/AllEvents.js
+++ frontend/event-helper/src/components/AllEvents.js
@@ -9,9 +9,23 @@ import EventTile from "./EventTile"
 
 export default function AllEvents() {
 
+    const [events, setEvents] = useState([]);
     const [sortBy, setSortBy] = useState("name");
     const [asc, setAsc] = useState("ascending");
     const [phrase, setPhrase] = useState("");
+
+    const getEvents = async () =>{
+        let ascending = true;
+        if (asc === "descending")  ascending = false;
+        const response = await fetch(`http://localhost:8080/api/events/sort/${sortBy}&${ascending}&${phrase}`); //sort?sortBy=name&ascending=true
+        const data = await response.json();
+        setEvents(data);
+    }
+
+    useEffect(() => {
+        getEvents().then(r => console.log(r))
+
+    }, [sortBy, asc, phrase]);
 
 
 
