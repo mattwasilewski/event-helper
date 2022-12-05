d13612920a7aa16021cd4e3f81781800d8c296c4
MichalK96 <miniocha1206@wp.pl>
2022-12-05 12:23:45 +0100

    CORS fixed :)

--- frontend/event-helper/src/pages/EventPage.js
+++ frontend/event-helper/src/pages/EventPage.js
@@ -17,7 +17,7 @@ export default function EventPage() {
     }, []);
 
     const getEvents = async () =>{
-        const response = await fetch(`http://localhost:8080/api/events/${id}`, {
+        const response = await fetch(`http://localhost:3000/api/events/${id}`, {
             method: 'GET',
         });
         const data = await response.json();
@@ -36,7 +36,7 @@ export default function EventPage() {
                 eventId: id,
                 userId: "af8afa53-4d00-4482-9758-c174b238dddb" })
         }
-        fetch('http://localhost:8080/api/events/assign-user-to-event', requestOptions)
+        fetch('http://localhost:3000/api/events/assign-user-to-event', requestOptions)
             .then(response => console.log(response.status))
     }
 
@@ -58,7 +58,7 @@ export default function EventPage() {
                 eventId: id,
                 description: document.getElementById("hah").innerText })
         }
-        fetch('http://localhost:8080/api/events/edit-event-description', requestOptions)
+        fetch('http://localhost:3000/api/events/edit-event-description', requestOptions)
             .then(response => console.log(response.status))
         setEditable("false");
         setButton(editButton);
--- src/main/java/com/codecool/CodeCoolProjectGrande/event/service/EventServiceImpl.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/event/service/EventServiceImpl.java
@@ -80,39 +80,6 @@ public class EventServiceImpl implements EventService {
     }
 
 
-    public List<Event> sortEvents(String sortBy, boolean ascending, String phrase, int page, int size) {
-        if (ascending) {
-            return eventRepository.findAllByNameContainingOrDescriptionContaining(phrase, phrase,
-                    PageRequest.of(page, size), Sort.by(sortBy).ascending());
-        }
-        return eventRepository.findAllByNameContainingOrDescriptionContaining(phrase, phrase,
-                PageRequest.of(page, size), Sort.by(sortBy).descending());
-    }
-
-
-    public ResponseEntity<?> assignUserToEvent(Map data) {
-        Optional<Event> event = eventRepository.findEventByEventId(UUID.fromString(String.valueOf(data.get("eventId"))));
-        Optional<User> user = userRepository.findUserByUserId(UUID.fromString(String.valueOf(data.get("userId"))));
-        //TODO czy zwracac responseentity/ true/false
-        if (event.isPresent() && user.isPresent()) {
-            event.get().assignUser(user.get());
-            eventRepository.save(event.get());
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-
-    public ResponseEntity<?> editEventDescriptionByEventId(Map data){
-        Optional<Event> event = eventRepository.findEventByEventId(UUID.fromString(String.valueOf(data.get("eventId"))));
-        System.out.println(data.get("description"));
-        if(event.isPresent()){
-            event.get().setDescription(String.valueOf(data.get("description")));
-            eventRepository.save(event.get());
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
 
 
 }
