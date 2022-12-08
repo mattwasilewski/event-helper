5a360103ffd7452c0ac5b4faec2dbbdaf4cedf91
Matt <mattwasilewski96@gmail.com>
2022-12-08 11:09:46 +0100

    event page component in progress

--- frontend/event-helper/src/pages/EventPage.js
+++ frontend/event-helper/src/pages/EventPage.js
@@ -1,22 +1,27 @@
-import React,{useEffect,useState} from "react";
-import logo from "../assets/logociemne.png";
+import React, {useEffect, useState} from "react";
 import "../EventPage.css"
-import switchMode from "../assets/dark-switch.png"
-import eventDj from "../assets/dj.png"
 import img from "../assets/login-img.png";
 import {useParams} from "react-router-dom";
+import Navbar from "../components/utils/Navbar";
+import Footer from "../components/utils/Footer";
+import CalendarTile from "../components/utils/CalendarTile";
+import SubscribePanel from "../components/utils/SubscribePanel";
+import ReadMoreReact from 'read-more-react';
+import {Container} from "@mui/material";
 
 export default function EventPage() {
 
-    let { id } = useParams()
+
+    let {id} = useParams()
     const [event, setEvent] = useState([]);
+
 
     useEffect(() => {
         getEvents().then(r => console.log(r))
 
     }, []);
 
-    const getEvents = async () =>{
+    const getEvents = async () => {
         const response = await fetch(`http://localhost:3000/api/events/${id}`, {
             method: 'GET',
         });
@@ -29,12 +34,15 @@ export default function EventPage() {
         e.preventDefault()
         const requestOptions = {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
                 'Access-Control-Allow-Origin': 'http://localhost:3000',
-                'Access-Control-Allow-Credentials': 'true'},
+                'Access-Control-Allow-Credentials': 'true'
+            },
             body: JSON.stringify({
                 eventId: id,
-                userId: "af8afa53-4d00-4482-9758-c174b238dddb" })
+                userId: "af8afa53-4d00-4482-9758-c174b238dddb"
+            })
         }
         fetch('http://localhost:3000/api/events/assign-user-to-event', requestOptions)
             .then(response => console.log(response.status))
@@ -42,7 +50,7 @@ export default function EventPage() {
 
     const [editable, setEditable] = useState(["false"]);
     const saveButton = <button onClick={(e) => editEventDescription(e)}>Save</button>;
-    const enableEditing = () =>{
+    const enableEditing = () => {
         setEditable("true");
         setButton(saveButton)
     }
@@ -51,12 +59,15 @@ export default function EventPage() {
     const editEventDescription = async () => {
         const requestOptions = {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
                 'Access-Control-Allow-Origin': 'http://localhost:3000',
-                'Access-Control-Allow-Credentials': 'true'},
+                'Access-Control-Allow-Credentials': 'true'
+            },
             body: JSON.stringify({
                 eventId: id,
-                description: document.getElementById("hah").innerText })
+                description: document.getElementById("hah").innerText
+            })
         }
         fetch('http://localhost:3000/api/events/edit-event-description', requestOptions)
             .then(response => console.log(response.status))
@@ -64,39 +75,32 @@ export default function EventPage() {
         setButton(editButton);
     }
 
+
     return (
         <div id="event-page">
-            <div id="logo">
-                <img src={logo} alt="logo"/>
-            </div>
-            <div id="frame19">
-                <div id="top-nav">
-                    <p id="nav-home"><a href="/home">Home</a></p>
-                    <p id="nav-events"><a href="/events">Events</a></p>
-                    <p id="nav-community"><a href="/community">Community</a></p>
-                    <p id="nav-about-us">About us</p>
-                    <p id="nav-login">Login</p>
-                    <p id="nav-sign-up"><a href="/register">Sign up</a></p>
-                    <div id="switch-mode">
-                        <img src={switchMode} alt=""/>
-                    </div>
-                </div>
-                <p id="event-name">{event.name}</p>
+            <Navbar/>
+            <p id="eventHeader">{event.name}</p>
+            <Container id="eventData">
+
                 {/*<p id="event-description">{event.description}</p>*/}
-
-                <button id="event-description" onClick={(e) => assignToEvent(e)}>Join to event</button>
-
-            </div>
-            <div id="destination">
-                <img id="rectangle-14" src={eventDj} alt=""/>
-                <div id="card-2">
-                    <div id="rectangle-15">
-                        <p>{event.location}</p>
-                        <label id="hah" contentEditable={editable}>{event.description}</label>
-                        {button}
-                    </div>
+                <button id="event-join" onClick={(e) => assignToEvent(e)}>Join to event</button>
+                <div>
+                    <img id="eventImg" src={event.logo} alt=""/>
+                    <p>{event.location}</p>
                 </div>
-            </div>
+
+                <Container id="eventText">
+                <p contentEditable={editable} dangerouslySetInnerHTML={{__html: event.description}}></p>
+                    <button id="edit-description">{button}</button>
+                </Container>
+
+
+
+            </Container>
+
+            <CalendarTile/>
+            <SubscribePanel/>
+            <Footer/>
         </div>
     );
 
--- src/main/java/com/codecool/CodeCoolProjectGrande/event/service/EventServiceImpl.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/event/service/EventServiceImpl.java
@@ -106,7 +106,7 @@ public class EventServiceImpl implements EventService {
                 event.offer.title,
                 event.offer.longDescription,
                 event.offer.url,
-                String.format("%s, %s", event.address.street, event.address.street),
+                String.format("%s, %s", event.address.street, event.address.city),
                 event.offer.mainImage.standard,
                 EventType.CONCERT,
                 event.startDate,
