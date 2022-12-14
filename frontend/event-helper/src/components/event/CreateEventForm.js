e6000530d2909dc04a8d246802b794a1dcb3979b
KZwolski <kkzwolski@gmail.com>
2022-12-14 11:44:38 +0100

    Modified CreateEventForm.js

--- frontend/event-helper/src/components/event/CreateEventForm.js
+++ frontend/event-helper/src/components/event/CreateEventForm.js
@@ -3,8 +3,11 @@ import React, {useState} from "react";
 import "../../css/App.css"
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import EventTile from "./EventTile";
+import Footer from "../utils/Footer";
 
 const API_URl = "http://localhost:3000/api/events/";
+
 function CreateEventForm() {
     let navigate = useNavigate();
     const [name, setName] = useState("");
@@ -19,7 +22,7 @@ function CreateEventForm() {
 
 
     const handleInputChange = (e) => {
-        const {id , value} = e.target;
+        const {id, value} = e.target;
         switch (id) {
             case "name":
                 setName(value)
@@ -51,11 +54,11 @@ function CreateEventForm() {
         }
     }
 
-    const handleSubmit  = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8'},
+            headers: {'Content-Type': 'application/json; charset=UTF-8'},
             body: JSON.stringify({
                 name: name,
                 startDate: startDate,
@@ -66,7 +69,8 @@ function CreateEventForm() {
                 description: description,
                 price: price,
                 publicEvent: publicEvent,
-                eventStatus: "TO_VERIFICATION"})   // TODO add userId after login implementation
+                eventStatus: "TO_VERIFICATION"
+            })   // TODO add userId after login implementation
         };
         // fetch('http://localhost:3000/api/events/create-event', requestOptions)
         //     .then(response => console.log(response.status))
@@ -75,63 +79,66 @@ function CreateEventForm() {
     }
 
     return (
-        <div className="form" >
-            <div>
-                <h1>Add new event</h1>
-                <div>
-                    <input type="text" value={name} onChange={(e) => handleInputChange(e)}
-                        id="name" className="input" placeholder="Name"/>
-                </div>
-                <div>
-                    <select id="eventType" className="input" value={eventType}
-                            onChange={(e) => handleInputChange(e)}>
-                        <option value="FESTIVAL">Festival</option>
-                        <option value="CONCERT">Concert</option>
-                        <option value="EXHIBITION">Exhibition</option>
-                        <option value="PARTY">Party</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="date" value={startDate} onChange={(e) => handleInputChange(e)}
-                        id="startDate" className="input"/>
-                </div>
-                <div>
-                    <input type="date" value={endDate} onChange={(e) => handleInputChange(e)}
-                           id="endDate" className="input"/>
-                </div>
-                <div>
-                    <input type="text" value={location} onChange={(e) => handleInputChange(e)}
-                        id="location" className="input" placeholder="Location"/>
-                </div>
-                <div>
-                    <input type="text" value={link} onChange={(e) => handleInputChange(e)}
-                        id="link" className="input" placeholder="Link to event page"/>
-                </div>
-                <div>
-                    <input type="number" value={price} onChange={(e) => handleInputChange(e)}
-                        id="price" className="input" placeholder="Price"/>
-                </div>
-                <div>
-                    <select id="publicEvent" className="input" value={publicEvent}
-                            onChange={(e) => handleInputChange(e)}>
-                        <option value="true">PRIVATE</option>
-                        <option value="false">PUBLIC</option>
-                    </select>
-                </div>
-                <div>
+        <>
+            <div className="add-event-form">
+                <h1>Create New Event</h1>
+                {/*<div>*/}
+                {/*    <div>*/}
+                <input type="text" value={name} onChange={(e) => handleInputChange(e)}
+                       id="name" className="input" placeholder="Name"/>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <select id="eventType" className="input" value={eventType}
+                        onChange={(e) => handleInputChange(e)}>
+                    <option value="FESTIVAL">Festival</option>
+                    <option value="CONCERT">Concert</option>
+                    <option value="EXHIBITION">Exhibition</option>
+                    <option value="PARTY">Party</option>
+                </select>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <input type="date" value={startDate} onChange={(e) => handleInputChange(e)}
+                       id="startDate" className="input"/>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <input type="date" value={endDate} onChange={(e) => handleInputChange(e)}
+                       id="endDate" className="input"/>
+                <input type="number" value={price} onChange={(e) => handleInputChange(e)}
+                       id="price" className="input" placeholder="Price"/>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <input type="text" value={location} onChange={(e) => handleInputChange(e)}
+                       id="location" className="input" placeholder="Location"/>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <input type="text" value={link} onChange={(e) => handleInputChange(e)}
+                       id="link" className="input" placeholder="Link to event page"/>
+                {/*    </div>*/}
+                {/*    <div>*/}
 
-                </div>
-                <div>
-                    <textarea value={description} onChange={(e) => handleInputChange(e)}
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <select id="publicEvent" className="input" value={publicEvent}
+                        onChange={(e) => handleInputChange(e)}>
+                    <option value="true">PRIVATE</option>
+                    <option value="false">PUBLIC</option>
+                </select>
+                {/*    </div>*/}
+                {/*    <div>*/}
+
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <textarea value={description} onChange={(e) => handleInputChange(e)}
                           id="description" className="big-input" placeholder="Description"/>
-                </div>
-                <div>
-                   <button onClick={(e) => handleSubmit(e)} type="submit" id="submit-btn" className="btn">
-                        Add Event
-                    </button>
-                </div>
+                {/*    </div>*/}
+                {/*    <div>*/}
+                <button onClick={(e) => handleSubmit(e)} type="submit" id="submit-btn" className="btn">
+                    Create Event
+                </button>
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
-        </div>
+        </>
     )
 }
 
