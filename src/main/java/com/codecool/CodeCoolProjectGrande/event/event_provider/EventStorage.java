e8c7796c84ae8c5c775f8ba540b7d45d04deca05
Matt <mattwasilewski96@gmail.com>
2023-01-08 14:14:58 +0100

    event provider package refactored

--- src/main/java/com/codecool/CodeCoolProjectGrande/event/event_provider/EventStorage.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/event/event_provider/EventStorage.java
@@ -2,10 +2,8 @@ package com.codecool.CodeCoolProjectGrande.event.event_provider;
 
 import com.codecool.CodeCoolProjectGrande.event.event_provider.wroclaw_model.ExternalEvent;
 import lombok.Data;
-
 import java.util.HashSet;
 import java.util.Set;
-
 
 @Data
 public class EventStorage {
--- src/main/java/com/codecool/CodeCoolProjectGrande/event/event_provider/wroclaw_model/Offer.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/event/event_provider/wroclaw_model/Offer.java
@@ -8,13 +8,7 @@ import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Data
-@NoArgsConstructor
 public class Offer implements Serializable {
 
     @JsonProperty("id")
