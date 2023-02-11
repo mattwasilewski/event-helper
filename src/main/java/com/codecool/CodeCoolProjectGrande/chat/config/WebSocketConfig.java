e6f0cd3e32f6659685e91c03ff35f8831c80bd22
KZwolski <kkzwolski@gmail.com>
2023-02-11 17:09:38 +0100

    package name changed

--- src/main/java/com/codecool/CodeCoolProjectGrande/chat/config/WebSocketConfig.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/chat/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.codecool.CodeCoolProjectGrande.chat.Config;
+package com.codecool.CodeCoolProjectGrande.chat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
